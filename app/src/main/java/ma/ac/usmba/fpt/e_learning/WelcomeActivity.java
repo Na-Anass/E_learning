751b1bff6081f6e6aaa95ae1b160ac642e61e2aa
erratbaoui <erratbaouiachraf@gmail.com>
2020-03-18 12:04:28 +0100

    Second commit

--- app/src/main/java/ma/ac/usmba/fpt/e_learning/WelcomeActivity.java
+++ app/src/main/java/ma/ac/usmba/fpt/e_learning/WelcomeActivity.java
@@ -2,8 +2,10 @@ package ma.ac.usmba.fpt.e_learning;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.VideoView;
 
 import java.net.URI;
@@ -14,9 +16,24 @@ public class WelcomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        playVideo();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        playVideo();
+    }
+    public void playVideo(){
         videoView = (VideoView) findViewById(R.id.welcome_video);
         String videopath =  "android.resource://"+getPackageName()+"/"+R.raw.welcome_video;
         videoView.setVideoPath(videopath);
         videoView.start();
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                videoView.start();
+            }
+        });
     }
 }
